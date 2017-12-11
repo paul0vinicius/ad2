e7c477d2094767e711191e982535ae8676e92397
Paulo Vinícius Soares <paulo.soares@ccc.ufcg.edu.br>
2017-12-11 09:49:48 -0300

    Lab03 start

---
+++ lab03/importa_dados_deputados.R
@@ -0,0 +1,10 @@
+get_dados_deputados <- function(){
+  read.csv("../database/eleicoes2014.csv", encoding = "latin1", stringsAsFactors = FALSE) %>%
+    mutate(# Replace dos campos com NA pela mediana para evitar a perda de dados.
+           recursos_de_outros_candidatos.comites = replace(recursos_de_outros_candidatos.comites, is.na(recursos_de_outros_candidatos.comites), 0),
+           recursos_de_partidos = replace(recursos_de_partidos, is.na(recursos_de_partidos), 0),
+           recursos_de_pessoas_físicas = replace(recursos_de_pessoas_físicas, is.na(recursos_de_pessoas_físicas), 0),
+           recursos_de_pessoas_juridicas = replace(recursos_de_pessoas_juridicas, is.na(recursos_de_pessoas_juridicas), 0),
+           recursos_proprios = replace(recursos_proprios, is.na(recursos_proprios), 0)
+    )
+}
